2203f3dcdaa1416e8a72014fef01f8f01bfd43ac
Jean Dubois <jd-dev@laposte.net>
2023-02-28 13:33:01 +0100

    update code

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ from string import ascii_letters as ascii_l
 from pprint import pprint
 import random
 
-ascii_letters = ascii_l[:26]
+LETTER_COMBINATIONS = FRENCH_LETTER_COMBINATIONS
+possible_letters = set()
+for couple in LETTER_COMBINATIONS:
+    for letter in couple:
+        possible_letters.add(letter.lower())
+print(possible_letters)
 
 
-def calculates_probability(couple):
+def calculate_probability(couple):
     total = 0
-    try:
-        if FRENCH_LETTER_COMBINATIONS[couple] is None:
-            return 0
-    except KeyError:
+    if LETTER_COMBINATIONS.get(couple) is None:
         return 0
-    for couple_ in FRENCH_LETTER_COMBINATIONS:
-        if FRENCH_LETTER_COMBINATIONS[couple_] is not None:
-            total += FRENCH_LETTER_COMBINATIONS[couple_]
+    for couple_ in LETTER_COMBINATIONS:
+        if LETTER_COMBINATIONS[couple_] is not None:
+            total += LETTER_COMBINATIONS[couple_]
     if total == 0:
         return 0
     else:
-        return (FRENCH_LETTER_COMBINATIONS[couple]*100) / total
+        return (LETTER_COMBINATIONS[couple]*100) / total
 
 
-letter = ascii_letters[random.randint(0, 25)]
-word = ''
 letters_in_the_word = input("How many letters in the word? ")
 try:
     letters_in_the_word = int(letters_in_the_word)
@@ -38,23 +38,28 @@ try:
 except Exception:
     words_number = 5
 
+word = ''
 words = []
 for word_ in range(words_number):
-    letter = ascii_letters[random.randint(0, 25)]
+    # possible_letters is a set
+    while (letter := random.choice(list(possible_letters))) in "-ôêàïöûëùèäü":  # totally arbitrary
+        letter = random.choice(list(possible_letters))
     word += letter
     for letter_ in range(letters_in_the_word):
-        letters = []
-        for loop in range(10):
-            letters.append(ascii_letters[random.randint(0, 25)])
         letter_list = []
-        for letter in letters:
+        for letter in list(possible_letters):
             if letter_ == letters_in_the_word - 1:
-                probability = calculates_probability(word[:1] + letter) * calculates_probability(letter) * 50
+                probability = calculate_probability(word[:1] + letter) + calculate_probability(letter) * 50
             else:
-                probability = calculates_probability(word[:1] + letter)
+                probability = calculate_probability(word[:1] + letter)
             for loop in range(int(probability * 500)):
                 letter_list.append(letter)
-        best_letter = random.choice(letter_list)
+        try:
+            best_letter = random.choice(letter_list)
+        except IndexError:
+            print(word)
+            print(letter_list)
+            raise
         word += best_letter
     words.append(word)
     word = ''
